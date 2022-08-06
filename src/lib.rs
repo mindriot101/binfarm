9ecd8356a19e0dea533f338347de8aaa822c152d
Simon Walker <s.r.walker101@googlemail.com>
2022-08-06 22:38:00 +0100

    feat: add ability to check for common crates

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,25 @@
+use std::collections::HashMap;
+
 use serde::Deserialize;
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct Manifest {
     #[serde(rename = "bin")]
     binaries: Option<Vec<BinaryDefinition>>,
+
+    dependencies: Option<HashMap<String, DependencyDefinition>>,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct BinaryDefinition {
     path: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+enum DependencyDefinition {
+    VersionOnly(String),
+    Complex(HashMap<String, String>),
 }
 
 fn is_cli_from_manifest(manifest: Manifest, files: &[&str]) -> bool {
@@ -26,6 +37,15 @@ fn is_cli_from_manifest(manifest: Manifest, files: &[&str]) -> bool {
     for path in files {
         if path.starts_with("src/bin/") {
             return true;
+        }
+    }
+
+    // common crates
+    if let Some(dependencies) = manifest.dependencies {
+        for dependency in &["clap", "structopt"] {
+            if dependencies.contains_key(*dependency) {
+                return true;
+            }
         }
     }
 
@@ -80,5 +100,32 @@ name = "test"
 "#;
 
         assert!(is_cli(contents, &["src/lib.rs", "src/bin/foo.rs"]));
+    }
+
+    #[test]
+    fn has_cli_crates() {
+        let examples = &[
+            r#"[package]
+            name = "test"
+
+            [dependencies]
+            clap = "0.1.0"
+            "#,
+            r#"[package]
+            name = "test"
+
+            [dependencies]
+            clap = { version = "0.1.0" }
+            "#,
+            r#"[package]
+            name = "test"
+
+            [dependencies]
+            structopt = "*"
+            "#,
+        ];
+        for contents in examples {
+            assert!(is_cli(contents, &[]));
+        }
     }
 }
