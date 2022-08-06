143d0178aa40bed46973ef592f930557320f6ef1
Simon Walker <s.r.walker101@googlemail.com>
2022-08-06 14:22:58 +0100

    feat: start is_cli function

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,20 @@
+fn is_cli(contents: impl AsRef<str>) -> bool {
+    true
+}
 
+#[cfg(test)]
+mod tests {
+    use super::*;
 
+    #[test]
+    fn binary_manifest() {
+        let contents = r#"[package]
+name = "test"
+
+[[bin]]
+name = "something"
+path = "src/bin/foo.rs"
+"#;
+        assert!(is_cli(contents));
+    }
+}
