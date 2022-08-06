fd946a48d0629a1b4364b3d8329d8c93dbf36541
Simon Walker <s.r.walker101@googlemail.com>
2022-08-06 14:30:00 +0100

    feat: support explicit binaries

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,28 @@
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Manifest {
+    #[serde(rename = "bin")]
+    binaries: Option<Vec<BinaryDefinition>>,
+}
+
+#[derive(Deserialize)]
+struct BinaryDefinition {
+    path: String,
+}
+
+fn is_cli_from_manifest(manifest: Manifest) -> bool {
+    if let Some(bin_definitions) = manifest.binaries {
+        return bin_definitions.len() > 0;
+    }
+
+    false
+}
+
 fn is_cli(contents: impl AsRef<str>) -> bool {
-    true
+    let manifest: Manifest =
+        toml::from_str(contents.as_ref()).expect("invalid manifest - could not parse toml");
+    is_cli_from_manifest(manifest)
 }
 
 #[cfg(test)]
@@ -7,7 +30,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn binary_manifest() {
+    fn explicit_binary() {
         let contents = r#"[package]
 name = "test"
 
@@ -15,6 +38,16 @@ name = "test"
 name = "something"
 path = "src/bin/foo.rs"
 "#;
+
         assert!(is_cli(contents));
+    }
+
+    #[test]
+    fn non_binary() {
+        let contents = r#"[package]
+name = "test"
+"#;
+
+        assert!(!is_cli(contents));
     }
 }
